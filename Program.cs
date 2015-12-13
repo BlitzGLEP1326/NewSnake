8903d7b65935eb13e5fb0f674f909e2b3d1401f3
sergeiGerman <sergei_german@icloud.com>
2015-12-13 18:46:02 +0300

    файл изменен ))

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace Snake
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Snake Console!!!");
+			int x1 = 1;
+			int y1 = 3;
+			char sym1 = '*';
+			Console.ReadLine ();
+			Console.SetCursorPosition (x1, y1);
+			Console.Write (sym1);
 		}
 	}
 }
